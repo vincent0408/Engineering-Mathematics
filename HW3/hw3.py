c4258fc82da90f4daa6c50f76d30b47d92b3b0cb
vincent0408 <tnecniv43208@gmail.com>
2023-05-09 22:15:34 +0800

    Update hw3.py

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -4,49 +4,33 @@ import matplotlib.pyplot as plt
 
 def problem_a():
     dx = 0.02
-    x = np.arange(dx, 4, dx)
+    x = np.arange(0, 4 - dx, dx)
     N = len(x)
     half = N // 2
     def gx(x):
         return np.sin(math.pi * x ** 3 / 8)
-    gdn = gx(x - 2)
+    gdn = gx(x - 1.98)
+    r = np.fft.fft(gdn)
     gdm = np.roll(np.fft.fft(gdn) * dx, half)
     f = np.arange(N) / dx / N
     f = np.concatenate((f[-half:] - 1/ dx, f[:-half]))
-    res = gdm * np.exp(1j * 2 * 2 * math.pi * f)
-    plt.plot(f, res.real), plt.plot(f, res.imag), plt.show()    
+    res = gdm * np.exp(1j * 2 * 1.98 * math.pi * f)
+    plt.figure(figsize=(20, 6)), plt.plot(f, res.real), plt.plot(f, res.imag), plt.show()    
 
     
 def problem_d():
     dx = 0.05
-    x = np.arange(dx, 6, dx)
+    x = np.arange(0, 6 - dx, dx)
     N = len(x)
     half = N // 2
     def gx(x):
         return x * np.exp(-np.abs(x) ** (3 / 2))
-    gdn = gx(x - 3)
+    gdn = gx(x - 2.95)
     gdm = np.roll(np.fft.fft(gdn) * dx, half)
     f = np.arange(N) / dx / N
     f = np.concatenate((f[-half:] - 1/ dx, f[:-half]))
-    res = gdm * np.exp(1j * 2 * 3 * math.pi * f)
-    plt.plot(f, res.real), plt.plot(f, res.imag), plt.show()    
+    res = gdm * np.exp(1j * 2 * 2.95 * math.pi * f)
+    plt.figure(figsize=(20, 6)), plt.plot(f, res.real), plt.plot(f, res.imag), plt.show()    
 
-    
-def test():
-    dx = 0.1
-    x = np.arange(0, 2+dx, dx)
-    N = len(x)
-    half = N // 2
-    def gx(x):
-        return (1 - np.abs(x)) ** 2
-    gdn = gx(x - 1)
-    gdm = np.roll(np.fft.fft(gdn) * dx, half)
-    f = np.arange(N) / dx / N
-    f = np.concatenate((f[-half:] - 1/ dx, f[:-half]))
-    res = gdm * np.exp(1j * 2 * math.pi *f)
-    plt.plot(f, res.real), plt.plot(f, res.imag), plt.show()
-
-
-# test()  
 problem_a()
 problem_d()
