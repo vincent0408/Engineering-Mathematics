5876b61b6b1ea2e7e221c64a014cde8dccc5599a
vincent0408 <tnecniv43208@gmail.com>
2023-05-09 19:12:55 +0800

    Update hw3.py

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -3,47 +3,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def problem_a():
-    delta_x = .02
-    N = int(4 / delta_x) - 1
-    half = (N - 1) // 2
+    dx = 0.02
+    x = np.arange(dx, 4, dx)
+    N = len(x)
+    half = N // 2
     def gx(x):
-        return np.sin(math.pi * (x-2) ** 3 / 8)
-    f = np.arange(-half, half+1, 1) / delta_x / N
-    gdn = gx(np.arange(0 + delta_x, 4, delta_x))
-    gdm = np.roll(np.fft.fft(gdn)* delta_x, (N - 1) // 2) * np.exp(4j * math.pi * f)
+        return np.sin(math.pi * x ** 3 / 8)
+    gdn = gx(x - 2)
+    gdm = np.roll(np.fft.fft(gdn) * dx, half)
+    f = np.arange(N) / dx / N
+    f = np.concatenate((f[-half:] - 1/ dx, f[:-half]))
+    res = gdm * np.exp(1j * 2 * 2 * math.pi * f)
+    plt.plot(f, res.real), plt.plot(f, res.imag), plt.show()    
 
-    plt.plot(f, gdm.real, 'o-')
-    plt.plot(f, gdm.imag, 'o-')
-
-    plt.show()
     
 def problem_d():
-    delta_x = .05
+    dx = 0.05
+    x = np.arange(dx, 6, dx)
+    N = len(x)
+    half = N // 2
     def gx(x):
-        return (x - 3) * np.exp(-np.abs((x - 3)) ** (3 / 2))
-    N = int(6 / delta_x) - 1
-    half = (N - 1) // 2
-    f = np.arange(-half, half+1, 1) / delta_x / N
-    
-    gdn = gx(np.arange(0+ delta_x, 6, delta_x))
-    gdm = np.roll(np.fft.fft(gdn)* delta_x, (N - 1) // 2) * np.exp(6j * math.pi * f)
-    plt.plot(f, gdm.real, 'o-')
-    plt.plot(f, gdm.imag, 'o-')
-    plt.show()
+        return x * np.exp(-np.abs(x) ** (3 / 2))
+    gdn = gx(x - 3)
+    gdm = np.roll(np.fft.fft(gdn) * dx, half)
+    f = np.arange(N) / dx / N
+    f = np.concatenate((f[-half:] - 1/ dx, f[:-half]))
+    res = gdm * np.exp(1j * 2 * 3 * math.pi * f)
+    plt.plot(f, res.real), plt.plot(f, res.imag), plt.show()    
 
     
 def test():
-    delta_x = .1
-    N = int(2 / delta_x) + 1
-    half = (N - 1) // 2
+    dx = 0.1
+    x = np.arange(0, 2+dx, dx)
+    N = len(x)
+    half = N // 2
     def gx(x):
-        return (1 - np.abs((x - 1))) ** 2
-    f = np.arange(-half, half + 1, 1) / delta_x / N  
-    gdn = gx(np.arange(0, 2+delta_x,delta_x))
-    gdm = np.roll(np.fft.fft(gdn)* delta_x, (N - 1) // 2) * np.exp(2j * math.pi * f)
-    plt.plot(f, gdm.real, 'o-')
-    plt.plot(f, [0] * N)
-    plt.show()
+        return (1 - np.abs(x)) ** 2
+    gdn = gx(x - 1)
+    gdm = np.roll(np.fft.fft(gdn) * dx, half)
+    f = np.arange(N) / dx / N
+    f = np.concatenate((f[-half:] - 1/ dx, f[:-half]))
+    res = gdm * np.exp(1j * 2 * math.pi *f)
+    plt.plot(f, res.real), plt.plot(f, res.imag), plt.show()
+
 
 # test()  
 problem_a()
